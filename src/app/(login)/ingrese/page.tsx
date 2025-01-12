a08eafb0da7db727978d696a2093db157f86444c
unknown <arthurolemos4@gmail.com>
2025-01-12 14:35:18 -0500

    Formik y yup incluido en el login, para los formularios. Falta conectar con el backend, y falta hacer lo de registro, pero esto ya es una buena guía

--- src/app/(login)/ingrese/page.tsx
+++ src/app/(login)/ingrese/page.tsx
@@ -5,7 +5,7 @@ import LogInBox from "@/components/3.templates/0.LogInBox";
 export default function Login() {
   return (
     // Contenedor de la pagina completa
-    <div className="flex flex-1 flex-col items-center  bg-SecBlue w-full">
+    <div className="flex flex-1 flex-col items-center bg-gradient-to-b from-SecBlue to-white w-full">
       {/* Contenidos de la columna */}
       <div className="w-[75%] sm:w-[82%] md:w-[70%] lg:w-[52%] xl:w-[42%] 2xl:w-[35%] h-full mt-20">
         {/* Logo linkeado al homepage */}

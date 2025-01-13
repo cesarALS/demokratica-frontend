56532018a781ba1eee2f82059e3271d0e6285245
JulianHuertasUNAL <jhuertasd@unal.edu.co>
2025-01-13 11:18:01 -0500

    continuación tarjeta para cada valor

--- src/templates/0.atoms/6.ValoresCard.tsx
+++ src/templates/0.atoms/6.ValoresCard.tsx
@@ -1,5 +1,31 @@
-export function ValoresCard(){
-    return <h1>ValoresCard</h1>
+
+
+interface ValoresDataTypes {
+  icono: string;
+  valor: string;
+  description: string;
+}
+
+export function ValoresCard() {
+  const valoresData: ValoresDataTypes[] = [
+    { icono: "", valor: "Colaboración", description: "" },
+    { icono: "", valor: "Diálogo", description: "" },
+    { icono: "", valor: "Eficiencia", description: "" },
+    { icono: "", valor: "Orden", description: "" }
+  ];
+
+  return valoresData.map((valor, index) => {
+    return (
+      <div
+        className="flex flex-col items-center justify-center gap-5"
+        key={index}
+      >
+        <img src={valor.icono} alt="icono" className="w-20 h-20" />
+        <h1 className="font-bold text-2xl">{valor.valor}</h1>
+        <p className="text-center">{valor.description}</p>
+      </div>
+    );
+  });
 }
 
 export default ValoresCard;

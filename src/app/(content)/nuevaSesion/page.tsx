ae00b54e6fa34d186de8109dffafb4a8bbd406bd
David-Felipe <dafemaro123@gmail.com>
2025-02-01 15:24:45 -0500

    1-2-1-25-DFMR-TagInputRealizado

--- src/app/(content)/nuevaSesion/page.tsx
+++ src/app/(content)/nuevaSesion/page.tsx
@@ -3,6 +3,7 @@ import PlanShow from "@/components/0.atoms/4.PlanShow";
 import FechasConfigSesion from "@/components/1.molecules/5.FechasConfigSesion";
 import ConfDescription from "@/components/0.atoms/5.ConfDescription";
 import { sessionConfig } from "@/types/sessionConfig";
+import TagInput from "@/templates/1.molecules/5.TagInput";
 
 interface NuevaSesionProps {
   config?: sessionConfig;
@@ -36,17 +37,7 @@ export default function NuevaSesion({ config }: NuevaSesionProps) {
             {/* Descripción */}
             <ConfDescription />
             {/* Tags */}
-            <div className="flex flex-col gap-y-4">
-              <div className="text-xl">Tags:</div>
-              <div className="flex w-full flex-wrap items-center justify-center gap-2">
-                <div className="rounded-lg bg-PrimGray p-1 text-white">
-                  Sistemas de información
-                </div>
-                <div className="rounded-lg bg-PrimGray p-1 text-white">
-                  Sisinfo
-                </div>
-              </div>
-            </div>
+            <TagInput />
           </div>
           {/* Configuraciones derecha */}
           <div>{/* Participantes */}</div>

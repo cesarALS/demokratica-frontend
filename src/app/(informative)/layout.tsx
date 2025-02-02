64977b711ef80d54644c48203bafb1fc02096fb9
David-Felipe <dafemaro123@gmail.com>
2025-02-02 09:55:00 -0500

    2-2-25-DFMR-ChangeLayoutInformative

--- src/app/(informative)/layout.tsx
+++ src/app/(informative)/layout.tsx
@@ -8,19 +8,19 @@ export default function InformativeLayout({
 }>) {
   return (
     // Contenedor principal como un flexbox con altura mínima de pantalla
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       {/* Header siempre visible en la parte superior */}
       <Header />
 
-      {/* Contenido principal que ocupa al menos 100vh, pero puede crecer */}
-      <div className="flex-grow overflow-y-auto">
-        <main className="flex flex-col items-center h-full w-full">
+      {/* Contenido principal que ocupa el resto del vh, que tiene el background fijo y que tiene un overflow que permite scrollear el contenido hacía abajo*/}
+      <div className="flex h-[91.667vh] w-full flex-initial flex-col items-center overflow-y-auto bg-gradient-to-b from-SecBlue to-white">
+        {/* Contenido de la página */}
+        <main className="flex min-h-[91.667vh] w-full flex-shrink-0 grow items-start">
           {children}
         </main>
+        {/* Footer al final de la página o contenido */}
+        <Footer />
       </div>
-
-      {/* Footer al final de la página o contenido */}
-      <Footer />
     </div>
   );
 }

7cbb58d550489eaeb007cfb5e86d3219157fd54d
David-Felipe <dafemaro123@gmail.com>
2025-02-02 20:06:10 -0500

    2-2-25-DFMR-NewSessionAlmostFinishedNotResponsive

--- src/app/(content)/nuevaSesion/page.tsx
+++ src/app/(content)/nuevaSesion/page.tsx
@@ -1,13 +1,12 @@
 import EditableTitle from "@/components/0.atoms/3.EditableTitle";
-import PlanShow from "@/components/0.atoms/4.PlanShow";
-import FechasConfigSesion from "@/components/1.molecules/5.FechasConfigSesion";
-import ConfDescription from "@/components/0.atoms/5.ConfDescription";
-import TagInput from "@/templates/1.molecules/5.TagInput";
 import { sessionConfig } from "@/types/sessionConfig";
 import ParticipantsBox from "@/components/3.templates/2.ParticipantsBox";
+import LeftSettingsNewSession from "@/components/3.templates/3.LeftSettingsNewSession";
+import SimpleButton from "@/templates/0.atoms/11.SimpleButton";
 
 export default function NuevaSesion() {
   // Prop de ejemplo
+  // La verda hay que cuadrar mucho mejor esto de los props, aunque para eso primero se necesitan las APIS
   const defaultConfig: sessionConfig = {
     title: "Ingresa tu titulo",
     plan: 0,
@@ -20,23 +19,19 @@ export default function NuevaSesion() {
       {/* Ingresar titulo */}
       <EditableTitle title={config.title} />
       {/* Configuraciones */}
-      <div className="flex flex-col rounded-lg border border-2 border-black bg-white p-6">
+      <div className="flex flex-col gap-y-6 rounded-lg border border-2 border-black bg-white p-6">
         {/* Grid para acomodar todo en responsive */}
         <div className="grid grid-rows-2 gap-y-6">
           {/* Configuraciones izquierda */}
-          <div className="flex flex-col gap-y-6">
-            {/* Plan actual */}
-            <PlanShow plan={config.plan} />
-            {/* Fechas */}
-            <FechasConfigSesion />
-            {/* Descripción */}
-            <ConfDescription />
-            {/* Tags */}
-            <TagInput />
-          </div>
+          <LeftSettingsNewSession />
           {/* Configuraciones derecha */}
           {/* Participantes */}
           <ParticipantsBox />
+        </div>
+        {/* Cancelar para volver a la pagina anterior o guardar */}
+        <div className="flex items-center justify-center gap-x-8 py-2 text-xl">
+          <SimpleButton buttonText="Cancelar" className="bg-PrimCasablanca" />
+          <SimpleButton buttonText="Guardar" className="bg-PrimCreamCan" />
         </div>
       </div>
     </div>

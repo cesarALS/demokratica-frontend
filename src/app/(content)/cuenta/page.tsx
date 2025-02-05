86321959ff8fa375cf6b28746e3488cd21ea0db4
arojasag <arojasag@unal.edu.co>
2025-02-04 23:24:47 -0500

    Parte visual de la pantalla grande ya casi terminada. Ya estoy usando un poquito el AuthContext para extraer el nombre de usuario

--- src/app/(content)/cuenta/page.tsx
+++ src/app/(content)/cuenta/page.tsx
@@ -1,7 +1,38 @@
-const Cuenta = () => {
+"use client"
+
+import { UserCircleIcon } from "@heroicons/react/24/solid";
+import { useAuthContext } from "@/utils/AuthProvider";
+
+export default function Cuenta() {
+    const {user} = useAuthContext();
+
     return (
-        <div>Hola mundo</div>
+        <div className="flex justify-center w-full text-center mt-6">
+            <div className="flex flex-col gap-y-8 w-[80%] sm:w-[20%] p-4 pb-10 mt-10 bg-white border-2 border-PrimBlack rounded-lg">
+                <div className = "text-3xl pt-4">
+                    Gestiona tu cuenta
+                </div>
+                <div className = "flex justify-center">
+                    <UserCircleIcon className="h-32 w-32 text-PrimGray" />
+                </div>
+                <div className = "flex flex-col gap-y-6 px-4">
+                    <AccountButton text = {!!user ? user?.username : "error"}/>
+                    <AccountButton text = {"Cambiar contraseña"}/>
+                    <AccountButton text = {"Eliminar cuenta"}/>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Cuenta;
+interface AccountButtonProps {
+    text: string
+}
+
+const AccountButton = ({text}: AccountButtonProps) => {
+    return (
+        <div className = "flex w-full justify-center text-2xl py-3 font-bold italic border-1.5 border-PrimBlack bg-ThirdGray rounded-lg">
+            {text}
+        </div>
+    );
+}

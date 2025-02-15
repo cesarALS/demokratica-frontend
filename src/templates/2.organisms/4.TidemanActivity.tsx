e942099ae6c16365e3d51e62bc165cc15a031b91
JulianHuertasUNAL <jhuertasd@unal.edu.co>
2025-02-15 12:25:56 -0500

    División contenido dos columnas actividad Tideman y pregunta centrada

--- src/templates/2.organisms/4.TidemanActivity.tsx
+++ src/templates/2.organisms/4.TidemanActivity.tsx
@@ -1,6 +1,6 @@
 import SimpleButton from "../0.atoms/11.SimpleButton";
 import ActivityHeader from "../1.molecules/9.ActivityHeader";
-
+import GridTwoColsRow from "./3.GridTwoColsRow";
 interface TidemanActivityProps {
   mode: string;
   tags: string[];
@@ -16,16 +16,28 @@ export default function TidemanActivity({
 
   if (mode === "resultados") {
     contenido = (
-      <div className="rounded-md border border-black bg-white p-4 text-center text-xl font-bold">
-        Recuadro para mostrar los resultados
-      </div>
+      <>
+        <GridTwoColsRow>
+          <div className="flex items-center justify-center rounded-md border border-black bg-white p-4 text-center text-2xl">
+            {pregunta}
+          </div>
+          <div className="rounded-md border border-black bg-white p-4 text-center text-xl">
+            Recuadro para mostrar los resultadosRecuadro para mostrar los resultadosRecuadro para mostrar los resultadosRecuadro para mostrar los resultadosRecuadro para mostrar los resultadosRecuadro para mostrar los resultados
+          </div>
+        </GridTwoColsRow>
+      </>
     );
   } else if (mode === "participar") {
     contenido = (
       <>
-        <div className="rounded-md border border-black bg-white p-4 text-center text-xl font-bold">
-          Recuadro para participar
-        </div>
+        <GridTwoColsRow>
+          <div className="flex items-center justify-center rounded-md border border-black bg-white p-4 text-center text-2xl ">
+            {pregunta}
+          </div>
+          <div className="rounded-md border border-black bg-white p-4 text-center text-xl">
+            Recuadro para participarRecuadro para participarRecuadro para participarRecuadro para participarRecuadro para participarRecuadro para participarRecuadro para participarRecuadro para participarRecuadro para participar
+          </div>
+        </GridTwoColsRow>
         <div className="flex items-center justify-center gap-x-4 py-2 text-xl">
           <SimpleButton
             buttonText="Enviar"
@@ -38,9 +50,6 @@ export default function TidemanActivity({
   return (
     <div className="flex flex-col gap-y-4 rounded-md border border-black bg-ThirdGray p-4">
       <ActivityHeader tags={tags} />
-      <div className="rounded-md border border-black bg-white p-4 text-center text-2xl font-bold">
-        {pregunta}
-      </div>
       {contenido}
     </div>
   );

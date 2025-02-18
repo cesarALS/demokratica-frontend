84b172eda5905103d320b265787a3ff10e76a817
David-Felipe <dafemaro123@gmail.com>
2025-02-18 16:33:59 -0500

    18-2-25-DFMR-ImplementedMarkdwonPrevInTextAreaTitle

--- src/templates/0.atoms/16.TextAreaTitle.tsx
+++ src/templates/0.atoms/16.TextAreaTitle.tsx
@@ -1,19 +1,43 @@
-interface TextAreaTitleMarkdownProps {
+"use client";
+
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeRaw from "rehype-raw";
+
+interface TextAreaMarkdownTitle {
   title: string;
   placeholder: string;
 }
 
-export default function TextAreaTitle({
+export default function TextAreaMarkdownTitle({
   title,
   placeholder,
-}: TextAreaTitleMarkdownProps) {
+}: TextAreaMarkdownTitle) {
+  const [markdown, setMarkdown] = useState("");
+
   return (
     <div className="flex flex-col items-start justify-start gap-y-4 text-xl">
       {title}
       <textarea
         className="w-full rounded-lg border-2 border-PrimGray bg-ThirdGray p-2 text-lg leading-tight text-PrimBlack placeholder-PrimBlack focus:outline-none focus:ring-1 focus:ring-PrimBlack"
         placeholder={placeholder}
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
       ></textarea>
+      {/* Vista previa */}
+      <div className="text-lg">Vista previa:</div>
+      <div className="w-full rounded-lg border-2 border-PrimGray bg-ThirdGray p-4">
+        <div className="prose prose-li:marker:text-PrimBlack max-w-none">
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw, rehypeSanitize]}
+          >
+            {markdown}
+          </ReactMarkdown>
+        </div>
+      </div>
     </div>
   );
 }

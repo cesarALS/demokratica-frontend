a25f4eaeab34916e4aba44aee4f163f3b1f761d9
David-Felipe <dafemaro123@gmail.com>
2025-02-19 09:29:01 -0500

    19-2-25-DFMR-VotaciónComnMdoPregnta

--- src/app/(content)/sesion/[idSesion]/page.tsx
+++ src/app/(content)/sesion/[idSesion]/page.tsx
@@ -1,9 +1,12 @@
 import PageContentContainer from "@/templates/2.organisms/1.PageContentContainer";
 import SessionTitleControls from "@/components/1.molecules/17.SessionTitleControls";
-import ContentCard from "@/templates/2.organisms/2.ContentCard";
-import TidemanActivity from "@/templates/2.organisms/4.TidemanActivity";
+import TextPublication from "@/templates/2.organisms/4.TextPublication";
 
+import ContentCard from "@/templates/2.organisms/2.ContentCard";
 import ActivityHeader from "@/templates/1.molecules/9.ActivityHeader";
+import MarkdownShower from "@/templates/0.atoms/18.MarkdownShower";
+import SelectableOptions from "@/templates/1.molecules/11.SelectableOptions";
+import SimpleButton from "@/templates/0.atoms/11.SimpleButton";
 
 export default function Sesion() {
   // Inside the publication
@@ -20,23 +23,16 @@ export default function Sesion() {
       {/* Controles y Titulo de la sesión */}
       <SessionTitleControls />
       {/* TODO: Aqui debe ir un componente que procese la lista de actividades y las renderice según su tipo */}
+      <TextPublication tags={tags} />
       <ContentCard>
         <ActivityHeader tags={tags} />
-      </ContentCard>
-      <ContentCard>
-        <TidemanActivity
-          mode="resultados"
-          tags={[
-            "tag1",
-            "tag2",
-            "Sistemas de información",
-            "Ingeniería de Software",
-            "tag1",
-            "tag2",
-            "Sistemas de información",
-            "Ingeniería de Software",
-          ]}
-          pregunta="¿Ejemplo de enunciado de pregunta?"
+        <MarkdownShower markdown={"Wenas, soy una pregunta"} />
+        <SelectableOptions
+          options={["Wenas", "adios", "uff, nais", "otra mas", "y otra"]}
+        />
+        <SimpleButton
+          buttonText="Enviar"
+          className="bg-PrimCreamCan hover:bg-SecCreamCan sm:max-w-[50%] sm:self-center"
         />
       </ContentCard>
     </PageContentContainer>

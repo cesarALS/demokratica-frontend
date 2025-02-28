eb85eea652272203a7f9d9e0f7c193772794b998
JulianHuertasUNAL <jhuertasd@unal.edu.co>
2025-02-27 20:32:13 -0500

    fix(activities): Corrige renderizado de las actividades por el nuevo formato GET de las actividades

--- src/utils/ContextProviders/SessionActivitiesStore.tsx
+++ src/utils/ContextProviders/SessionActivitiesStore.tsx
@@ -1,5 +1,11 @@
 import { create } from "zustand";
 
+export interface PollResult {
+  id?: number; // Puede ser null en el JSON, lo hacemos opcional
+  description: string; // Puede ser null en el JSON
+  numVotes: number;
+}
+
 type Activity = {
   id: number;
   title: string;
@@ -7,16 +13,22 @@ type Activity = {
   startTime: string;
   endTime: string;
   tags: { text: string }[];
-  pollOptions?: { id: number; description: string; voters: string[] }[];
+  pollResults?: PollResult[]; // Cambiado de pollOptions a pollResults
   type: string;
+  activityStatus: string; // Agregar el estado de la actividad
+  alreadyParticipated: boolean; // Agregar si el usuario ya participó
 };
 
 type SessionActivitiesStore = {
   activities: Activity[];
+  userRole: string | null;
   setActivities: (activities: Activity[]) => void;
+  setUserRole: (role: string) => void;
 };
 
 export const useSessionActivitiesStore = create<SessionActivitiesStore>((set) => ({
   activities: [],
+  userRole: null,
   setActivities: (activities) => set({ activities }),
+  setUserRole: (role) => set({ userRole: role }),
 }));
